57dc170f4c804cd8365d7bd06f6c72bfb2af5001
ukkary <ukkary@gmail.com>
2020-04-10 22:39:48 +0900

    use page action

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+var color = '';
+var uuid = '';
+var token = '';
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
@@ -7,4 +11,34 @@ chrome.runtime.onInstalled.addListener(function() {
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
+});
+
+// type: 1 get security token
+// type: 2 get color code
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+
+  if (request.type === 1) {
+    uuid = request.data.uuid;
+    token = request.data.token;
+  }
+
+  if (request.type === 2) {
+    color = request.color;
+    var charUrl = 'https://apiv5.openrec.tv/api/v5/users/me/chat-setting';
+    var data = {"name_color": color}
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('PUT', charUrl, true);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.setRequestHeader('access-token', token);
+    xhr.setRequestHeader('uuid', uuid);
+    xhr.send(JSON.stringify(data));
+    xhr.onerror = function() {
+      alert('失敗したようです、プレミアムアカウントでログインしていることを確認して、リロードして試してみてください');
+    }
+    xhr.onload = function() {
+      alert('成功したようです');
+    }
+  }
+
 });
